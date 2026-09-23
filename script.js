76856f5ec96edb007fd3e5b43f787c6d9693c565
carnocentaurus <carnocentaurus@gmail.com>
2026-09-23 16:59:41 +0800

    Store each diagonals to a variable

--- script.js
+++ script.js
@@ -9,6 +9,9 @@ function createGrid() {
     const column2 = grid.filter(item => item === 1 || item === 4 || item === 7);
     const column3 = grid.filter(item => item === 2 || item === 5 || item === 8);
 
+    const diagonal1 = grid.filter(item => item === 0 || item === 4 || item === 8);
+    const diagonal2 = grid.filter(item => item === 2 || item === 4 || item === 6);
+
     console.log(grid[0], grid[1], grid[2]);
     console.log(grid[3], grid[4], grid[5]);
     console.log(grid[6], grid[7], grid[8]);
