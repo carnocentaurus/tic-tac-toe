5500426c689546f46d2278731916fdde541e66b4
carnocentaurus <carnocentaurus@gmail.com>
2026-09-23 14:35:46 +0800

    Update createGrid() function to use grid[] array

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 function createGrid() {
-    console.log(1, 2, 3);
-    console.log(4, 5, 6);
-    console.log(7, 8, 9);
+    const grid = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+
+    console.log(grid[0], grid[1], grid[2]);
+    console.log(grid[3], grid[4], grid[5]);
+    console.log(grid[6], grid[7], grid[8]);
 }
 
 createGrid();
